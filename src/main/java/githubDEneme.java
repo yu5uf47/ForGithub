166027a76eb150f2f3a16eca09ba9ce6293b3eed
yu5uf47 <yusufalas103@gmail.com>
2023-10-03 16:39:31 -0400

    v3

--- src/main/java/githubDEneme.java
+++ src/main/java/githubDEneme.java
@@ -1,5 +1,8 @@
 public class githubDEneme {
     public static void main(String[] args) {
         System.out.println("Hello Word");
+        System.out.println("Hello Word");
+        System.out.println("Hello Word");
+        System.out.println("Hello Word");
     }
 }
